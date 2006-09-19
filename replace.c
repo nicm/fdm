09b2a6172aa443dff4c7c2ee8fb567611f8b1af8
Nicholas Marriott <nicm@users.sf.net>
2006-09-19 15:54:46 +0000

    Fix stupid overflow bug.

--- replace.c
+++ replace.c
@@ -59,8 +59,11 @@ replace(char *src, char *map[REPL_LEN])
 		switch (*ptr) {
 		case '%':
 			ch = *++ptr;
-			if (ch == '\0')
-				break;
+			if (ch == '\0') {
+				ENSURE_SIZE(dst, len, off + 1);
+				dst[off++] = '%';
+				goto out;
+			}
 			rp = NULL;
 			if (REPL_IDX(ch) != -1)
 				rp = map[REPL_IDX(ch)];
@@ -83,6 +86,7 @@ replace(char *src, char *map[REPL_LEN])
 		}
 	}
 	
+out:
 	ENSURE_SIZE(dst, len, off + 1);
 	dst[off] = '\0';
 
