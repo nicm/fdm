e5ff1f438b4388eb4ce34ac0617dc683c6bbd819
Nicholas Marriott <nicm@users.sf.net>
2007-08-30 21:25:49 +0000

    cat is actually unnecessary.

--- regress/deliver/test-subexpression1.sh
+++ regress/deliver/test-subexpression1.sh
@@ -14,6 +14,5 @@ EOF
 
 cat <<EOF|test_run
 match "^:(.) (.) (.) (.) (.) (.) (.) (.) (.) (.)"
-	action rewrite "cat >/dev/null;" +
-	"echo %0; echo %1 %2 %3 %4 %5 %6 %7 %8 %9 %10" continue
+	action rewrite "echo %0; echo %1 %2 %3 %4 %5 %6 %7 %8 %9 %10" continue
 EOF
