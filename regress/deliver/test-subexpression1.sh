dd45dd188bb824050293610220f242d45d4b2bad
Nicholas Marriott <nicm@users.sf.net>
2007-08-30 21:24:51 +0000

    Tests for $0-%9 subexpressions.

---
+++ regress/deliver/test-subexpression1.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+# $Id$
+
+. ./test-deliver.subr && test_init
+
+cat <<EOF|test_in
+:0 1 2 3 4 5 6 7 8 9
+EOF
+
+cat <<EOF|test_out
+:0 1 2 3 4 5 6 7 8 9
+0 1 2 3 4 5 6 7 8 00
+EOF
+
+cat <<EOF|test_run
+match "^:(.) (.) (.) (.) (.) (.) (.) (.) (.) (.)"
+	action rewrite "cat >/dev/null;" +
+	"echo %0; echo %1 %2 %3 %4 %5 %6 %7 %8 %9 %10" continue
+EOF
